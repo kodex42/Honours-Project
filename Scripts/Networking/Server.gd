1a002086fe8028a7cef1daf5a480c6ca77360af8
Matthew Buchanan <matt@localhost.localdomain>
2021-07-27 16:12:37 -0400

    Basic multiplayer server implementation as placeholder

--- Scripts/Networking/Server.gd
+++ Scripts/Networking/Server.gd
@@ -1,4 +1,22 @@
-extends HTTPRequest
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var port = 1909
+var max_players = 100
 
 func _ready():
-	print("Server Mode selected")
+	start_server()
+
+func start_server():
+	network.create_server(port, max_players)
+	get_tree().set_network_peer(network)
+	print("Server started")
+	
+	network.connect("peer_connected", self, "_peer_connected")
+	network.connect("peer_disconnected", self, "_peer_disconnected")
+
+func _peer_connected(pid):
+	print("User " + str(pid) + " Connected")
+
+func _peer_disconnected(pid):
+	print("User " + str(pid) + " Disconnected")
