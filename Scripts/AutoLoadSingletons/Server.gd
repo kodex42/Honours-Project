54ce82cc5f9c816acf11c357725f9e4aba0589dd
Matt Buchanan <mbkodex42@hotmail.com>
2021-07-27 16:00:29 -0400

    AutoLoaded server connection singleton script created

---
+++ Scripts/AutoLoadSingletons/Server.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+#var ip = "127.0.0.1" # Loopback
+var ip = "192.168.0.143" # Laptop at home on WLAN
+var port = 1909
+
+func _ready():
+	connect_to_server()
+
+func connect_to_server():
+	network.create_client(ip, port)
+	get_tree().set_network_peer(network)
+	
+	network.connect("connection_failed", self, "_on_connection_failed")
+	network.connect("connection_succeeded", self, "_on_connection_succeeded")
+
+func _on_connection_failed():
+	print("Failed to connect to server")
+
+func _on_connection_succeeded():
+	print("Successfully connected to server")
--- Scripts/Networking/WorldStateRequester.gd
+++ Scripts/Networking/WorldStateRequester.gd
@@ -1,4 +1,4 @@
-extends HTTPRequest
+extends Node
 
 func _ready():
 	pass # Replace with function body.
